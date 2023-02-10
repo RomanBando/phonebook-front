03ccb8cee533eaff7b35c50b77037dd21077e77a
Roman <109812065+RomanBando@users.noreply.github.com>
2023-02-10 11:00:44 +0200

    some changes to style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,13 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import Image from 'react-bootstrap/Image';
 import getFilter from '../utils/getFilter';
+import './Contact.css';
 
 export default function Contact({contact, handleOpen}) {
   const [photoFilterStyle, photoFilterAmount] = contact?.photoFilter?.split(',') || [];
 
   return (
-    <ListGroup.Item onClick={handleOpen}>
+    <ListGroup.Item className='Contact' onClick={handleOpen}>
       <Image 
         src={contact.photo} 
         style={{filter: getFilter(photoFilterStyle, photoFilterAmount)}} 
